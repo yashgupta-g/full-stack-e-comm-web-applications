1af8897f5dc29022d676764d9c47485d8d4a068e
yash <52443096+yashgupta-g@users.noreply.github.com>
2023-07-30 05:12:15 +0530

    template addes

---
+++ react-ecommerce/src/Pages/Homepage.js
@@ -0,0 +1,17 @@
+import Nav from "../features/Nav/Nav";
+import ProductList from "../features/Product-list/ProductList";
+function Homepage () {
+    return ( 
+<div>
+
+<Nav> 
+    <ProductList></ProductList>
+</Nav>
+</div>
+
+
+
+     );
+}
+
+export default Homepage ;
