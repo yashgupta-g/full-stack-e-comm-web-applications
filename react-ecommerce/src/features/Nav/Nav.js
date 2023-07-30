3d7699b97a10d5748821814d9574c276663fddca
yash <52443096+yashgupta-g@users.noreply.github.com>
2023-07-30 23:50:12 +0530

    pagination added

--- react-ecommerce/src/features/Nav/Nav.js
+++ react-ecommerce/src/features/Nav/Nav.js
@@ -10,11 +10,11 @@ const user = {
     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 }
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
+  { name: 'Men', href: '#', current: true },
+  { name: 'Women', href: '#', current: false },
+  { name: 'Kids', href: '#', current: false },
+  { name: 'More', href: '#', current: false },
+  { name: 'Sale', href: '#', current: false },
 ]
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
