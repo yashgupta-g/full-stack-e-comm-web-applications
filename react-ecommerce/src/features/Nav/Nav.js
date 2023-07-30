1be33ea42235b1f7854c11e1d9f4d646c39e0d61
yash <52443096+yashgupta-g@users.noreply.github.com>
2023-07-31 00:09:24 +0530

    added colors

--- react-ecommerce/src/features/Nav/Nav.js
+++ react-ecommerce/src/features/Nav/Nav.js
@@ -29,18 +29,14 @@ function Nav ({children}) {
     return (
         <>
         <div className="min-h-full">
-          <Disclosure as="nav" className="bg-gray-800">
+          <Disclosure as="nav" className="bg-teal-800">
             {({ open }) => (
               <>
                 <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                   <div className="flex h-16 items-center justify-between">
                     <div className="flex items-center">
                       <div className="flex-shrink-0">
-                        <img
-                          className="h-8 w-8"
-                          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                          alt="Your Company"
-                        />
+                        <h1 className='  text-2xl text-white font-bold'>GZ</h1>
                       </div>
                       <div className="hidden md:block">
                         <div className="ml-10 flex items-baseline space-x-4">
--- react-ecommerce/src/features/Product-list/ProductList.js
+++ react-ecommerce/src/features/Product-list/ProductList.js
@@ -321,7 +321,7 @@ export default function ProductList() {
 
 {/* this is product page */}
 <div className=" bg-slate-900">
-    <div className="mx-auto max-w-2xl px-4 py-0 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 bg-teal-300">
+    <div className="mx-auto max-w-2xl px-4 py-0 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 bg-teal-100">
 
       <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8 ">
         {products.map((product) => (
